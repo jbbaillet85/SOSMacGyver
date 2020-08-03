e3dc20a9fe1b3150034c6007e5adf974fd9f73dc
Jean-baptiste Baillet <jbbaillet.dev@gmail.com>
2020-08-03 17:06:43 +0200

    chargement du labyrinthe, Macgyver et le gardien

--- game.py
+++ game.py
@@ -3,22 +3,34 @@
 
 import pygame
 from constants import *
-from wall import *
 from labyrinth import *
 from hero import *
 
 pygame.init()
 
-#Open the windows of Pygame
 screen = pygame.display.set_mode((SPRITE_HEIGHT, SPRITE_WIDTH))
-#Title
 pygame.display.set_caption("SOS MacGyver")
 
 background = pygame.image.load("pictures/meadow.jpg")
 
-#labyrinth = Labyrinth()
-#labyrinth.create_labyrinth()
-#labyrinth.display_labyrinth(screen)
+MacGyver = pygame.image.load("pictures/MacGyver32.png").convert_alpha()
+Gardian = pygame.image.load("pictures/Gardien32.png").convert_alpha()
+wall = pygame.image.load("pictures/wall32.jpg").convert_alpha()
+num_line = 0
+with open("labyrinth.txt", "r") as file:
+    for line in file:
+        num_box = 0
+        for sprite in line:
+            x = num_box*SPRITE_SIZE
+            y = num_line*SPRITE_SIZE
+            if sprite == "W":
+                background.blit(wall,(x, y))
+            elif sprite == "S":
+                background.blit(MacGyver,(x,y))
+            elif sprite == "A":
+                background.blit(Gardian,(x,y))
+            num_box += 1
+        num_line += 1
 
 pygame.display.flip()
 
@@ -30,6 +42,7 @@ while True:
 
     #Affichages aux nouvelles positions
     screen.blit(background, (0,0))
+    
     #labyrinth.display_labyrinth(screen)
     GROUP_GLOBAL_SPRITES.update()
     GROUP_GLOBAL_SPRITES.draw(screen)
