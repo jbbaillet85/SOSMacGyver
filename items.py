bc6702642ad9586005bac27cc66767bec1b4f961
Jean-baptiste Baillet <jbbaillet.dev@gmail.com>
2020-08-05 18:37:13 +0200

    ajout des paramètres img_path et name

--- items.py
+++ items.py
@@ -2,14 +2,15 @@
 #coding utf-8
 
 import pygame
-import constants
 import random
+from constants import *
 
 class Items(pygame.sprite.Sprite):
     """Create items for labyrinth"""
-    def __init__(self, x,y):
+    def __init__(self, img_path, name):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("pictures/ether32.jpg")
+        self.image = pygame.image.load(img_path)
+        self.name = name
         self.rect = self.image.get_rect()
         self.rect.y = random.randint(0, SPRITE_NUMBER-1)*SPRITE_SIZE
         self.rect.x = random.randint(0, SPRITE_NUMBER-1)*SPRITE_SIZE
