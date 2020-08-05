dc02d0ad42d15e74f37d4f303e05b1dd3623fd74
Jean-baptiste Baillet <jbbaillet.dev@gmail.com>
2020-08-05 18:34:38 +0200

    ajout des paramètres img_path et name

--- hero.py
+++ hero.py
@@ -6,11 +6,10 @@ from constants import *
 
 class Hero(pygame.sprite.Sprite):
     """Create hero for labyrinth"""
-    def __init__(self):
+    def __init__(self, img_path, name):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("pictures/MacGyver32.png").convert_alpha()
+        self.image = pygame.image.load(img_path).convert_alpha()
+        self.name = name
         self.rect = self.image.get_rect()
-        self.rect.y = SPRITE_SIZE*y
-        self.rect.x = SPRITE_SIZE*x
-    
-    
+        self.rect.y = SPRITE_SIZE
+        self.rect.x = SPRITE_SIZE
