ad86bc38d621d5e36c1b14e5264302f1841ff9a2
Jean-baptiste Baillet <jbbaillet.dev@gmail.com>
2020-08-05 18:35:55 +0200

    ajout des paramètres img_path et name et supression de x et y

--- guardian.py
+++ guardian.py
@@ -6,9 +6,10 @@ from constants import *
 
 class Gardian(pygame.sprite.Sprite):
     """Create gardian for labyrinth"""
-    def __init__(self):
+    def __init__(self, img_path, name):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.load.image("pictures/Gardien32.png").convert_alpha()
+        self.image = pygame.image.load(img_path).convert_alpha()
+        self.name = name
         self.rect = self.image.get_rect()
-        self.rect.y = SPRITE_SIZE*y
-        self.rect.x = SPRITE_SIZE*x
+        self.rect.y = SPRITE_SIZE
+        self.rect.x = SPRITE_SIZE
