245339083f2136eeda13262d930fc321b573d53d
Jean-baptiste Baillet <jbbaillet.dev@gmail.com>
2020-09-02 18:21:54 +0200

    ajout wall dans groupe walls

--- game.py
+++ game.py
@@ -4,6 +4,7 @@
 import pygame
 import random
 from constants import *
+from wall import *
 from hero import *
 from guardian import *
 from items import *
@@ -17,7 +18,8 @@ background = pygame.image.load("pictures/meadow.jpg")
 
 MacGyver = Hero("pictures/MacGyver32.png", "MacGyver")
 Gardian = Gardian("pictures/Gardien32.png", "Gardian")
-wall = pygame.image.load("pictures/wall32.jpg").convert_alpha()
+
+wall = Wall("pictures/wall32.jpg")
 
 aiguille = Items("pictures/seringue32.png", "aiguille")
 tube = Items("pictures/tube_plastique32.png", "tube")
@@ -31,15 +33,21 @@ with open("labyrinth.txt", "r") as file:
             x = num_box*SPRITE_SIZE
             y = num_line*SPRITE_SIZE
             if sprite == "W":
-                background.blit(wall,(x, y))
+                background.blit(wall.image,(x, y))
+                GROUP_WALLS.add(wall)
+                GROUP_GLOBAL_SPRITES.add(wall)
             elif sprite == "S":
                 background.blit(MacGyver.image,(x,y))
+                GROUP_GLOBAL_SPRITES.add(MacGyver)
             elif sprite == "A":
                 background.blit(Gardian.image,(x,y))
+                GROUP_GLOBAL_SPRITES.add(Gardian)
             elif sprite == "0":
                 background.blit(aiguille.image, (aiguille.rect.x, aiguille.rect.y))
                 background.blit(tube.image, (tube.rect.x, tube.rect.y))
                 background.blit(ether.image, (ether.rect.x, ether.rect.y))
+                GROUP_ITEMS.add(aiguille, tube, ether)
+                GROUP_GLOBAL_SPRITES.add(aiguille, tube, ether)
             num_box += 1
         num_line += 1
 
@@ -48,11 +56,7 @@ pygame.display.flip()
 #Loop of game
 while True:
 
-    #Limitation de vitesse de la boucle
     pygame.time.Clock().tick(30)
-
-    #Affichages aux nouvelles positions
-    screen.blit(background, (0,0))
     
     GROUP_GLOBAL_SPRITES.update()
     GROUP_GLOBAL_SPRITES.draw(screen)
@@ -62,18 +66,16 @@ while True:
         if event.type == pygame.QUIT:
             False
             pygame.quit()
+
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                MacGyver.move = "L"
-                break
+                MacGyver.move("L")
             elif event.key == pygame.K_RIGHT:
-                MacGyver.move = "R"
-                break
+                MacGyver.move("R")
             elif event.key ==pygame.K_UP:
-                MacGyver.move = "U"
-                break
+                MacGyver.move("U")
             elif event.key == pygame.K_DOWN:
-                MacGyver.move = "D"
-                break
-    
-        MacGyver.update()
+                MacGyver.move("D")
+
+    screen.blit(background, (0,0))
+    pygame.display.flip()
--- hero.py
+++ hero.py
@@ -13,3 +13,29 @@ class Hero(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.y = SPRITE_SIZE
         self.rect.x = SPRITE_SIZE
+        self.direction = "0"
+    
+    def move(self):
+        x_current = self.rect.x
+        y_current = self.rect.y
+        if self.move == "L":
+            self.rect.x -= SPRITE_SIZE
+            self.direction = "0"
+        elif self.move == "R":
+            self.rect.x += SPRITE_SIZE
+            self.direction = "0"
+        elif self.move == "U":
+            self.rect.y -= SPRITE_SIZE
+            self.direction = "0"
+        elif self.move == "D":
+            self.rect.y += SPRITE_SIZE
+            self.direction = "0"
+        
+        liste_collision_wall = pygame.sprite.spritecollide(self, GROUP_WALLS, False)
+        if len(liste_collision_wall)>0:
+            self.rect.x = x_current
+            self.rect.y = y_current
+        
+        liste_collision_items = pygame.sprite.spritecollide(self, GROUP_ITEMS, False)
+        for items in liste_collision_items:
+            items.kill()
